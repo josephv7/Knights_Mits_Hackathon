f7866f8acd06aa642f358e193f441459d5c6ba69
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-03-17 10:28:58 +0530

    fetching and uploading using history api....samples working

--- Vita/app/src/main/java/com/knights/vita/Fit.java
+++ Vita/app/src/main/java/com/knights/vita/Fit.java
@@ -16,7 +16,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.data.Subscription;
@@ -27,6 +30,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -35,6 +39,9 @@ import java.util.concurrent.TimeUnit;
 import logger.LogView;
 import logger.LogWrapper;
 import logger.MessageOnlyLogFilter;
+
+import static java.text.DateFormat.getDateInstance;
+import static java.text.DateFormat.getTimeInstance;
 
 public class Fit extends AppCompatActivity {
 
@@ -55,6 +62,24 @@ public class Fit extends AppCompatActivity {
         initializeLogging();
 
         button = findViewById(R.id.button);
+
+
+
+        FitnessOptions fitnessOptions =
+                FitnessOptions.builder()
+                        .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_WRITE)
+                        .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_WRITE)
+                        .build();
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this,
+                    REQUEST_OAUTH_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+        } else {
+            insertData();
+            Log.d("calling","insert data");
+        }
 
 
         Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
@@ -93,28 +118,13 @@ public class Fit extends AppCompatActivity {
             public void onClick(View view) {
 //                readData();
 //                accessGoogleFit();
-
-
+                readHistoryData();
 
 
 
             }
         });
 
-        FitnessOptions fitnessOptions =
-                FitnessOptions.builder()
-                        .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
-                        .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
-                        .build();
-        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
-            GoogleSignIn.requestPermissions(
-                    this,
-                    REQUEST_OAUTH_REQUEST_CODE,
-                    GoogleSignIn.getLastSignedInAccount(this),
-                    fitnessOptions);
-        } else {
-            subscribe();
-        }
     }
 
     @Override
@@ -172,22 +182,7 @@ public class Fit extends AppCompatActivity {
                         });
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the main; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if (id == R.id.action_read_data) {
-//            readData();
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+
 
     /** Initializes a custom log class that outputs both to in-app targets and logcat. */
     private void initializeLogging() {
@@ -217,47 +212,201 @@ public class Fit extends AppCompatActivity {
         cal.add(Calendar.YEAR, -1);
         long startTime = cal.getTimeInMillis();
 
+        java.text.DateFormat dateFormat = getDateInstance();
+        Log.i(TAG, "Range Start: " + dateFormat.format(startTime));
+        Log.i(TAG, "Range End: " + dateFormat.format(endTime));
+
         DataReadRequest readRequest = new DataReadRequest.Builder()
                 .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
-                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
                 .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
                 .build();
 
 
-//        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
-//                .readData(readRequest)
-//                .addOnSuccessListener(new OnSuccessListener() {
-//                    @Override
-//                    public void onSuccess(DataReadResponse dataReadResponse) {
-//                        Log.d(LOG_TAG, "onSuccess()");
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.e(LOG_TAG, "onFailure()", e);
-//                    }
-//                })
-//                .addOnCompleteListener(new OnCompleteListener() {
-//                    @Override
-//                    public void onComplete(@NonNull Task task) {
-//                        Log.d(LOG_TAG, "onComplete()");
-//                    }
-//                });
+
+        final Task<DataReadResponse> response = Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)).readData(readRequest);
 
 
-        Fitness.getHistoryClient(getApplicationContext(),GoogleSignIn.getLastSignedInAccount(this))
-                .readData(readRequest)
-                .addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
-                    @Override
-                    public void onSuccess(DataReadResponse dataReadResponse) {
-                        Log.d("data",dataReadResponse.getDataSets().toString());
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
+
+        response.addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
             @Override
-            public void onFailure(@NonNull Exception e) {
+            public void onSuccess(DataReadResponse dataReadResponse) {
+//                List<DataSet> dataSets = dataReadResponse.getResult().getDataSets();
+//
+//                Log.d("inside size",Integer.toString(dataSets.size()));
 
+//                dumpDataSet(dataSets.get(0));
             }
         });
+
     }
+
+
+
+    private static void dumpDataSet(DataSet dataSet) {
+        Log.i(TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
+        DateFormat dateFormat = getTimeInstance();
+
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            Log.i(TAG, "Data point:");
+            Log.i(TAG, "\tType: " + dp.getDataType().getName());
+            Log.i(TAG, "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            Log.i(TAG, "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
+            for (Field field : dp.getDataType().getFields()) {
+                Log.i(TAG, "\tField: " + field.getName() + " Value: " + dp.getValue(field));
+            }
+        }
+    }
+
+
+    private DataSet insertFitnessData() {
+        Log.i(TAG, "Creating a new data insert request.");
+
+        Log.d("inside","insertfitnessdata");
+
+        // [START build_insert_data_request]
+        // Set a start and end time for our data, using a start time of 1 hour before this moment.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.HOUR_OF_DAY, -1);
+        long startTime = cal.getTimeInMillis();
+
+        // Create a data source
+        DataSource dataSource =
+                new DataSource.Builder()
+                        .setAppPackageName(this)
+                        .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                        .setStreamName(TAG + " - step count")
+                        .setType(DataSource.TYPE_RAW)
+                        .build();
+
+        // Create a data set
+        int stepCountDelta = 950;
+        DataSet dataSet = DataSet.create(dataSource);
+        // For each data point, specify a start time, end time, and the data value -- in this case,
+        // the number of new steps.
+        DataPoint dataPoint =
+                dataSet.createDataPoint().setTimeInterval(startTime, endTime, TimeUnit.MILLISECONDS);
+        dataPoint.getValue(Field.FIELD_STEPS).setInt(stepCountDelta);
+        dataSet.add(dataPoint);
+        // [END build_insert_data_request]
+
+        return dataSet;
+    }
+
+    private Task<Void> insertData() {
+
+        Log.d("inisde","insert data");
+        // Create a new dataset and insertion request.
+        DataSet dataSet = insertFitnessData();
+
+        Log.d("after insertfitnessdata",dataSet.toString());
+
+        // Then, invoke the History API to insert the data.
+        Log.i(TAG, "Inserting the dataset in the History API.");
+        return Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .insertData(dataSet)
+                .addOnCompleteListener(
+                        new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    // At this point, the data has been inserted and can be read.
+                                    Log.i(TAG, "Data insert was successful!");
+                                } else {
+                                    Log.e(TAG, "There was a problem inserting the dataset.", task.getException());
+                                }
+                            }
+                        });
+    }
+
+
+
+    private Task<DataReadResponse> readHistoryData() {
+        // Begin by creating the query.
+        DataReadRequest readRequest = queryFitnessData();
+
+        // Invoke the History API to fetch the data with the query
+        return Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .readData(readRequest)
+                .addOnSuccessListener(
+                        new OnSuccessListener<DataReadResponse>() {
+                            @Override
+                            public void onSuccess(DataReadResponse dataReadResponse) {
+                                // For the sake of the sample, we'll print the data so we can see what we just
+                                // added. In general, logging fitness information should be avoided for privacy
+                                // reasons.
+                                printData(dataReadResponse);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e(TAG, "There was a problem reading the data.", e);
+                            }
+                        });
+    }
+
+
+    public static void printData(DataReadResponse dataReadResult) {
+        // [START parse_read_data_result]
+        // If the DataReadRequest object specified aggregated data, dataReadResult will be returned
+        // as buckets containing DataSets, instead of just DataSets.
+        if (dataReadResult.getBuckets().size() > 0) {
+            Log.i(
+                    TAG, "Number of returned buckets of DataSets is: " + dataReadResult.getBuckets().size());
+            for (Bucket bucket : dataReadResult.getBuckets()) {
+                List<DataSet> dataSets = bucket.getDataSets();
+                for (DataSet dataSet : dataSets) {
+                    dumpDataSet(dataSet);
+                }
+            }
+        } else if (dataReadResult.getDataSets().size() > 0) {
+            Log.i(TAG, "Number of returned DataSets is: " + dataReadResult.getDataSets().size());
+            for (DataSet dataSet : dataReadResult.getDataSets()) {
+                dumpDataSet(dataSet);
+            }
+        }
+        // [END parse_read_data_result]
+    }
+
+
+    public static DataReadRequest queryFitnessData() {
+        // [START build_read_data_request]
+        // Setting a start and end date using a range of 1 week before this moment.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+        java.text.DateFormat dateFormat = getDateInstance();
+        Log.i(TAG, "Range Start: " + dateFormat.format(startTime));
+        Log.i(TAG, "Range End: " + dateFormat.format(endTime));
+
+        DataReadRequest readRequest =
+                new DataReadRequest.Builder()
+                        // The data request can specify multiple data types to return, effectively
+                        // combining multiple data queries into one call.
+                        // In this example, it's very unlikely that the request is for several hundred
+                        // datapoints each consisting of a few steps and a timestamp.  The more likely
+                        // scenario is wanting to see how many steps were walked per day, for 7 days.
+                        .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                        // Analogous to a "Group By" in SQL, defines how data should be aggregated.
+                        // bucketByTime allows for a time span, whereas bucketBySession would allow
+                        // bucketing by "sessions", which would need to be defined in code.
+                        .bucketByTime(1, TimeUnit.DAYS)
+                        .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                        .build();
+        // [END build_read_data_request]
+
+        return readRequest;
+    }
+
+
+
 }
