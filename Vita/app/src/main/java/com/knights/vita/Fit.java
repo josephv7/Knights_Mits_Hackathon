6a08d1a1ff59773e4b14385c9a2aa26be37f00a2
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-03-17 05:51:36 +0530

    new activity created....google fit dependencies added

---
+++ Vita/app/src/main/java/com/knights/vita/Fit.java
@@ -0,0 +1,26 @@
+package com.knights.vita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataType;
+
+public class Fit extends AppCompatActivity {
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fit);
+
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .build();
+
+
+    }
+}
--- Vita/app/src/main/java/com/knights/vita/Splash.java
+++ Vita/app/src/main/java/com/knights/vita/Splash.java
@@ -12,9 +12,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.JsonObject;
-import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.ion.Ion;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
