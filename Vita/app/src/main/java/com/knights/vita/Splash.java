5e73f81277865ca89aea49c5be7e998e5ac12e0c
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-03-17 15:28:35 +0530

    added handler to splash screen....edited manifest

--- Vita/app/src/main/java/com/knights/vita/Splash.java
+++ Vita/app/src/main/java/com/knights/vita/Splash.java
@@ -1,5 +1,7 @@
 package com.knights.vita;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,7 +12,13 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntnet = new Intent(Splash.this,Home.class);
+                startActivity(homeIntnet);
+            }
+        },300);
 
     }
 }
